d33e644d342d1643c94672106c5508dafcb8ef07
perrykoradiya <perrykoradiya99@gmail.com>
2024-04-16 13:50:44 -0400

    commit2

---
+++ src/Components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="container"><div className="row">
+        <div className="col-lg-12">
+            <h1>Footer Here</h1>
+        </div>
+    </div></div>
+  )
+}
+
+export default Footer
